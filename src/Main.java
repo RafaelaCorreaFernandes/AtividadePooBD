04aa9e84a92df3308c9684395522af00a304063a
Rafaela Fernandes <rafaela63466666@alunos.sc.senac.br>
2026-07-16 14:18:01 -0300

    menu para cadastro de produtos

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,52 @@
 import java.util.List;
- 
+import java.util.Scanner;
+
 import dao.ProdutoDao;
 import modelos.Produto;
  
 public class Main {
  
 	public static void main(String[] args) {
-		
-		Produto p1 = new Produto("Carne",20.85);
-      
+		Scanner sc = new Scanner(System.in);
 		ProdutoDao dao = new ProdutoDao();
+		int opcao;
 		
-		//Produto retorno = dao.salvar(p1);
-		
-		//System.out.println(retorno.getId() + " " + retorno.getDescricao());
-		
-		/*List<Produto> lista = dao.consultar();
-		for (Produto p : lista) {
-			System.out.println(p.getId() + " " + p.getDescricao());
-		}*/
-		
-		Produto p = dao.consultar(4);
-		System.out.println(p.getId() + " " + p.getDescricao() + " " +p.getDescricao());
-		}
- 
+		do {
+			System.out.println("\n===== MENU =====");
+			System.out.println("1 - Cadastrar produto");
+			System.out.println("0 - Sair");
+			System.out.print("Escolha uma opção: ");
+			opcao = sc.nextInt();
+			sc.nextLine(); // LIMPAR
+
+			switch (opcao) {
+				case 1:
+					System.out.print("Descrição do produto: ");
+					String descricao = sc.nextLine();
+					System.out.print("Preço: ");
+					double preco = sc.nextDouble();
+					sc.nextLine();
+
+					Produto novoProduto = new Produto(descricao, preco);
+					Produto salvo = dao.salvar(novoProduto);
+
+					if (salvo != null) {
+						System.out.println("Produto cadastrado com sucesso!");
+					} else {
+						System.out.println("Erro ao cadastrar produto.");
+					}
+					break;
+
+				case 0:
+					System.out.println("Saindo...");
+					break;
+
+				default:
+					System.out.println("Opção inválida!");
+			}
+
+		} while (opcao != 0);
+
+		sc.close();
+}
 }
